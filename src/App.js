93e71dd664e7de5033a17becd9e45d93388f04b0
sarahmacneill <sarah_f_macneill@yahoo.co.uk>
2021-05-10 23:48:18 +0100

    add buttons

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
+      <h1>Avatar Generator</h1>
       <Canvas />
     </div>
   );
